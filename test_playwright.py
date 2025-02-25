3ceb0fc30bf7673e882b618fadd823bc1071024a
masha0465 <pingpongvv@gmail.com>
2025-02-25 20:42:31 +0900

    Add files via upload

---
+++ test_playwright.py
@@ -0,0 +1,8 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto("https://example.com")
+    print(page.title())  # 웹 페이지 제목 출력
+    browser.close()
